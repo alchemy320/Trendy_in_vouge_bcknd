f1092be2ad537f3809a4e478b09f7902f216bb38
alchemy320 <kirkleader3@gmail.com>
2026-08-17 12:08:18 +0300

    push

--- trendy_in_vogue/asgi.py
+++ trendy_in_vogue/asgi.py
@@ -8,7 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import sys
+from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/alchemy/www/Trendy_in_vouge_bcknd/Trendy_in_vouge_bcknd')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Trendy_in_vouge_bcknd.settings')
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trendy_in_vogue.settings')
